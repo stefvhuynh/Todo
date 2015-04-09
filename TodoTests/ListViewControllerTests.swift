19086e5b689c71a45d4a5f3e981d3b78a3ef4093
Stefan Huynh <huynh.stefan@gmail.com>
2015-04-09 01:29:29 -0400

    Move controller tests to appropriate test file

---
+++ TodoTests/ListViewControllerTests.swift
@@ -0,0 +1,48 @@
+//
+//  ListViewControllerTests.swift
+//  TodoTests
+//
+//  Created by Stefan Huynh on 4/9/15.
+//  Copyright (c) 2015 Stefan Huynh. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ListViewControllerTests: XCTestCase {
+    var listViewController: ListViewController!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storyboard = UIStoryboard(
+            name: "Main",
+            bundle: NSBundle(forClass: self.dynamicType)
+        )
+        
+        listViewController =
+            storyboard.instantiateViewControllerWithIdentifier(
+                "ListViewControllerId"
+            ) as ListViewController
+        
+        let _ = listViewController.view
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testTableViewConnected() {
+        let tableView = listViewController.tableView
+        
+        XCTAssertNotNil(tableView, "tableView is not connected")
+        XCTAssertTrue(
+            tableView.delegate === listViewController,
+            "tableView.delegate is not set to listViewController"
+        )
+        XCTAssertTrue(
+            tableView.dataSource === listViewController,
+            "tableView.dataSource is not set to listViewController"
+        )
+    }
+}
--- TodoTests/TodoTests.swift
+++ TodoTests/TodoTests.swift
@@ -10,39 +10,11 @@ import UIKit
 import XCTest
 
 class TodoTests: XCTestCase {
-    var listViewController: ListViewController!
-    
     override func setUp() {
         super.setUp()
-        
-        let storyboard = UIStoryboard(
-            name: "Main",
-            bundle: NSBundle(forClass: self.dynamicType)
-        )
-        
-        listViewController =
-            storyboard.instantiateViewControllerWithIdentifier(
-                "ListViewControllerId"
-            ) as ListViewController
-        
-        let _ = listViewController.view
     }
     
     override func tearDown() {
         super.tearDown()
-    }
-    
-    func testTableViewConnected() {
-        let tableView = listViewController.tableView
-        
-        XCTAssertNotNil(tableView, "tableView is not connected")
-        XCTAssertTrue(
-            tableView.delegate === listViewController,
-            "tableView.delegate is not set to listViewController"
-        )
-        XCTAssertTrue(
-            tableView.dataSource === listViewController,
-            "tableView.dataSource is not set to listViewController"
-        )
     }
 }
